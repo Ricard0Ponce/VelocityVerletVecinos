fef08c43c0b81f934032a58d87b58b0a2a2f3d5d
Ricard0Ponce <ricardoponc932@gmail.com>
2024-11-13 12:57:15 -0600

    Version final

---
+++ cmake-build-debug/CMakeFiles/Ejercicio2.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/rich/Escritorio/NotasFC/VelocityVerletMejorado/Ejercicio2/Atomo.cpp" "CMakeFiles/Ejercicio2.dir/Atomo.cpp.o" "gcc" "CMakeFiles/Ejercicio2.dir/Atomo.cpp.o.d"
+  "/home/rich/Escritorio/NotasFC/VelocityVerletMejorado/Ejercicio2/Sim.cpp" "CMakeFiles/Ejercicio2.dir/Sim.cpp.o" "gcc" "CMakeFiles/Ejercicio2.dir/Sim.cpp.o.d"
+  "/home/rich/Escritorio/NotasFC/VelocityVerletMejorado/Ejercicio2/main.cpp" "CMakeFiles/Ejercicio2.dir/main.cpp.o" "gcc" "CMakeFiles/Ejercicio2.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Ejercicio2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicio2.dir/Atomo.cpp.o"
+  "CMakeFiles/Ejercicio2.dir/Atomo.cpp.o.d"
+  "CMakeFiles/Ejercicio2.dir/Sim.cpp.o"
+  "CMakeFiles/Ejercicio2.dir/Sim.cpp.o.d"
+  "CMakeFiles/Ejercicio2.dir/main.cpp.o"
+  "CMakeFiles/Ejercicio2.dir/main.cpp.o.d"
+  "Ejercicio2"
+  "Ejercicio2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicio2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
